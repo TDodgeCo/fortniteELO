705d9d0f256294cc399c606ea8aee466eb9fb942
Tim Dodge <timldodge@gmail.com>
2018-07-30 17:22:57 -0600

    hooked up to fortnite api

--- app/Controllers/Http/StatController.js
+++ app/Controllers/Http/StatController.js
@@ -1,6 +1,32 @@
 'use strict'
+const Env = use('Env')
+const Fortnite = use('fortnite-api')
 
 class StatController {
+  async index ({}) {
+    let fortniteAPI = new Fortnite(
+      [
+        Env.get('EMAIL'),
+        Env.get('PASS'),
+        Env.get('EPIC_CLIENT_TOKEN'),
+        Env.get('FN_CLIENT_TOKEN')
+      ],
+      {
+          debug: true
+      }
+    )
+
+    fortniteAPI.login().then(() => {
+      fortniteAPI
+          .getStatsBR("Mirardes", "pc", "weekly")
+          .then(stats => {
+              console.log(stats);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+    });
+  }
 }
 
 module.exports = StatController
